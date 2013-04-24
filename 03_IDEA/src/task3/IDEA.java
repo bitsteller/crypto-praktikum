53b6255a27e7ff37a0350aada6c836f6e352a2f5
Vincent Breitmoser <valodim@mugenguild.com>
2013-04-24 22:06:03 +0200

    idea: fix indentation

--- 03_IDEA/src/task3/IDEA.java
+++ 03_IDEA/src/task3/IDEA.java
@@ -27,66 +27,66 @@ import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
  */
 public final class IDEA extends BlockCipher {
 
-  /**
-   * Entschlüsselt den durch den FileInputStream <code>ciphertext</code>
-   * gegebenen Chiffretext und schreibt den Klartext in den FileOutputStream
-   * <code>cleartext</code>.
-   *
-   * @param ciphertext
-   * Der FileInputStream, der den Chiffretext liefert.
-   * @param cleartext
-   * Der FileOutputStream, in den der Klartext geschrieben werden soll.
-   */
-  public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
+    /**
+     * Entschlüsselt den durch den FileInputStream <code>ciphertext</code>
+     * gegebenen Chiffretext und schreibt den Klartext in den FileOutputStream
+     * <code>cleartext</code>.
+     *
+     * @param ciphertext
+     * Der FileInputStream, der den Chiffretext liefert.
+     * @param cleartext
+     * Der FileOutputStream, in den der Klartext geschrieben werden soll.
+     */
+    public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
 
-  }
+    }
 
-  /**
-   * Verschlüsselt den durch den FileInputStream <code>cleartext</code>
-   * gegebenen Klartext und schreibt den Chiffretext in den FileOutputStream
-   * <code>ciphertext</code>.
-   * 
-   * @param cleartext
-   * Der FileInputStream, der den Klartext liefert.
-   * @param ciphertext
-   * Der FileOutputStream, in den der Chiffretext geschrieben werden soll.
-   */
-  public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
+    /**
+     * Verschlüsselt den durch den FileInputStream <code>cleartext</code>
+     * gegebenen Klartext und schreibt den Chiffretext in den FileOutputStream
+     * <code>ciphertext</code>.
+     * 
+     * @param cleartext
+     * Der FileInputStream, der den Klartext liefert.
+     * @param ciphertext
+     * Der FileOutputStream, in den der Chiffretext geschrieben werden soll.
+     */
+    public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
 
-  }
+    }
 
-  /**
-   * Erzeugt einen neuen Schlüssel.
-   * 
-   * @see #readKey readKey
-   * @see #writeKey writeKey
-   */
-  public void makeKey() {
+    /**
+     * Erzeugt einen neuen Schlüssel.
+     * 
+     * @see #readKey readKey
+     * @see #writeKey writeKey
+     */
+    public void makeKey() {
 
-    System.out.println("Dummy für die Schlüsselerzeugung.");
-  }
+        System.out.println("Dummy für die Schlüsselerzeugung.");
+    }
 
-  /**
-   * Liest den Schlüssel mit dem Reader <code>key</code>.
-   * 
-   * @param key
-   * Der Reader, der aus der Schlüsseldatei liest.
-   * @see #makeKey makeKey
-   * @see #writeKey writeKey
-   */
-  public void readKey(BufferedReader key) {
+    /**
+     * Liest den Schlüssel mit dem Reader <code>key</code>.
+     * 
+     * @param key
+     * Der Reader, der aus der Schlüsseldatei liest.
+     * @see #makeKey makeKey
+     * @see #writeKey writeKey
+     */
+    public void readKey(BufferedReader key) {
 
-  }
+    }
 
-  /**
-   * Schreibt den Schlüssel mit dem Writer <code>key</code>.
-   * 
-   * @param key
-   * Der Writer, der in die Schlüsseldatei schreibt.
-   * @see #makeKey makeKey
-   * @see #readKey readKey
-   */
-  public void writeKey(BufferedWriter key) {
+    /**
+     * Schreibt den Schlüssel mit dem Writer <code>key</code>.
+     * 
+     * @param key
+     * Der Writer, der in die Schlüsseldatei schreibt.
+     * @see #makeKey makeKey
+     * @see #readKey readKey
+     */
+    public void writeKey(BufferedWriter key) {
 
-  }
+    }
 }
