fa376d4e6b39b7322fd36bd85459c448c253b7ee
Vincent Breitmoser <valodim@mugenguild.com>
2013-04-30 12:10:59 +0200

    idea: subkey generation routine

--- 03_IDEA/src/task3/IDEA.java
+++ 03_IDEA/src/task3/IDEA.java
@@ -18,6 +18,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
 import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
 
@@ -39,6 +40,7 @@ public final class IDEA extends BlockCipher {
      * of eight keys are created by rotating the main key left 25 bits
      * between each group of eight.
      *
+     * @param key 128 bit BigInteger
      * @return an array of 52 sequential subkeys
      *
      */
@@ -46,8 +48,43 @@ public final class IDEA extends BlockCipher {
         // there should be no bits set after the 128th! this is basically key.length == 16
         assert(key.and(_128bits).equals(key));
 
+        // allocate buffer space
+        ByteBuffer buf = ByteBuffer.allocate(104);
+
+        // BigInteger trickery: set 128th bit to make sure we get exactly 9
+        // bytes, then skip the first one
+        key = key.and(_128bits).setBit(128);
+        buf.put(key.toByteArray(), 1, 16);
+        // 8 keys + 6*8 keys = 56 keys
+        for(int i = 0; i < 6; i++) {
+            // shift left by 25 bits in 128-bit rotation, and filter first 128 bits only
+            key = key.and(_128bits).shiftRight(103).or(key.shiftLeft(25)).setBit(128);
+            // put 16 bytes, or 8 for the last round
+            buf.put(key.toByteArray(), 1, i < 5 ? 16 : 8);
+        }
+
+        // there should be no bytes left for writing!
+        assert(buf.remaining() == 0);
+
+        // ok, get back to beginning, and write first 104 bytes into an array of 52 shorts
         short[] ret = new short[52];
+        buf.flip();
+        buf.asShortBuffer().get(ret, 0, 52);
+
         return ret;
+    }
+
+    public static void main_subkeys(String[] args) {
+        // byte[] key = new byte[] { (byte) 0x42, (byte) 0x61, (byte) 0xce, (byte) 0xd1, (byte) 0xff, (byte) 0x55, (byte) 0xff, (byte) 0x1d,
+        //                           (byte) 0xf2, (byte) 0x12, (byte) 0xfc, (byte) 0xfa, (byte) 0xaa, (byte) 0xff, (byte) 0x91, (byte) 0xff };
+
+        assert(args.length == 1 && args[0].length() == 16);
+        short[] subkeys = IDEA.idea_subkeys(new BigInteger(args[0].getBytes()));
+
+        for(int i = 0; i < subkeys.length; i++) {
+            System.out.println(String.format("%04x", subkeys[i]));
+        }
+
     }
 
     /** One block of IDEA, consisting of 8.5 rounds of IDEA.
