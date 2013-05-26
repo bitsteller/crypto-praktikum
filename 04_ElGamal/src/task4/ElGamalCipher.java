7ee0facd9f283b3c42147802a1473d09f51f73e0
Vincent Breitmoser <valodim@mugenguild.com>
2013-05-26 11:07:42 +0200

    elgamal: indentation and some vars

--- 04_ElGamal/src/task4/ElGamalCipher.java
+++ 04_ElGamal/src/task4/ElGamalCipher.java
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
 
 import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
 
@@ -26,72 +27,77 @@ import de.tubs.cs.iti.jcrypt.chiffre.BlockCipher;
  */
 public final class ElGamalCipher extends BlockCipher {
 
-  /**
-   * Entschlüsselt den durch den FileInputStream <code>ciphertext</code>
-   * gegebenen Chiffretext und schreibt den Klartext in den FileOutputStream
-   * <code>cleartext</code>.
-   * <p>Das blockweise Lesen des Chiffretextes soll mit der Methode {@link
-   * #readCipher readCipher} durchgeführt werden, das blockweise Schreiben des
-   * Klartextes mit der Methode {@link #writeClear writeClear}.</p>
-   *
-   * @param ciphertext
-   * Der FileInputStream, der den Chiffretext liefert.
-   * @param cleartext
-   * Der FileOutputStream, in den der Klartext geschrieben werden soll.
-   */
-  public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
+    /**
+     * Entschlüsselt den durch den FileInputStream <code>ciphertext</code>
+     * gegebenen Chiffretext und schreibt den Klartext in den FileOutputStream
+     * <code>cleartext</code>.
+     * <p>Das blockweise Lesen des Chiffretextes soll mit der Methode {@link
+     * #readCipher readCipher} durchgeführt werden, das blockweise Schreiben des
+     * Klartextes mit der Methode {@link #writeClear writeClear}.</p>
+     *
+     * @param ciphertext
+     * Der FileInputStream, der den Chiffretext liefert.
+     * @param cleartext
+     * Der FileOutputStream, in den der Klartext geschrieben werden soll.
+     */
+    public void decipher(FileInputStream ciphertext, FileOutputStream cleartext) {
 
-  }
+    }
 
-  /**
-   * Verschlüsselt den durch den FileInputStream <code>cleartext</code>
-   * gegebenen Klartext und schreibt den Chiffretext in den FileOutputStream
-   * <code>ciphertext</code>.
-   * <p>Das blockweise Lesen des Klartextes soll mit der Methode {@link
-   * #readClear readClear} durchgeführt werden, das blockweise Schreiben des
-   * Chiffretextes mit der Methode {@link #writeCipher writeCipher}.</p>
-   * 
-   * @param cleartext
-   * Der FileInputStream, der den Klartext liefert.
-   * @param ciphertext
-   * Der FileOutputStream, in den der Chiffretext geschrieben werden soll.
-   */
-  public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
+    /**
+     * Verschlüsselt den durch den FileInputStream <code>cleartext</code>
+     * gegebenen Klartext und schreibt den Chiffretext in den FileOutputStream
+     * <code>ciphertext</code>.
+     * <p>Das blockweise Lesen des Klartextes soll mit der Methode {@link
+     * #readClear readClear} durchgeführt werden, das blockweise Schreiben des
+     * Chiffretextes mit der Methode {@link #writeCipher writeCipher}.</p>
+     * 
+     * @param cleartext
+     * Der FileInputStream, der den Klartext liefert.
+     * @param ciphertext
+     * Der FileOutputStream, in den der Chiffretext geschrieben werden soll.
+     */
+    public void encipher(FileInputStream cleartext, FileOutputStream ciphertext) {
 
-  }
+    }
 
-  /**
-   * Erzeugt einen neuen Schlüssel.
-   * 
-   * @see #readKey readKey
-   * @see #writeKey writeKey
-   */
-  public void makeKey() {
+    // did I do this public private thing right? :)
+    // public key part
+    public BigInteger g, p, q, h;
+    // private key part
+    private BigInteger x;
 
-    System.out.println("Dummy für die Schlüsselerzeugung.");
-  }
+    /**
+     * Erzeugt einen neuen Schlüssel.
+     * 
+     * @see #readKey readKey
+     * @see #writeKey writeKey
+     */
+    public void makeKey() {
+        System.out.println("Dummy für die Schlüsselerzeugung.");
+    }
 
-  /**
-   * Liest den Schlüssel mit dem Reader <code>key</code>.
-   * 
-   * @param key
-   * Der Reader, der aus der Schlüsseldatei liest.
-   * @see #makeKey makeKey
-   * @see #writeKey writeKey
-   */
-  public void readKey(BufferedReader key) {
+    /**
+     * Liest den Schlüssel mit dem Reader <code>key</code>.
+     * 
+     * @param key
+     * Der Reader, der aus der Schlüsseldatei liest.
+     * @see #makeKey makeKey
+     * @see #writeKey writeKey
+     */
+    public void readKey(BufferedReader key) {
 
-  }
+    }
 
-  /**
-   * Schreibt den Schlüssel mit dem Writer <code>key</code>.
-   * 
-   * @param key
-   * Der Writer, der in die Schlüsseldatei schreibt.
-   * @see #makeKey makeKey
-   * @see #readKey readKey
-   */
-  public void writeKey(BufferedWriter key) {
+    /**
+     * Schreibt den Schlüssel mit dem Writer <code>key</code>.
+     * 
+     * @param key
+     * Der Writer, der in die Schlüsseldatei schreibt.
+     * @see #makeKey makeKey
+     * @see #readKey readKey
+     */
+    public void writeKey(BufferedWriter key) {
 
-  }
+    }
 }
