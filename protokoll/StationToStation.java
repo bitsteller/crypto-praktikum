0a85919cbf09d6dcac353a26bbf189450dd5942e
Vincent Breitmoser <valodim@mugenguild.com>
2013-06-07 14:17:04 +0200

    s2s: turn some comments into code

--- protokoll/StationToStation.java
+++ protokoll/StationToStation.java
@@ -3,6 +3,10 @@
  */
 
 import de.tubs.cs.iti.jcrypt.protokoll.*;
+import de.tubs.cs.iti.jcrypt.chiffre.*;
+
+import java.math.*;
+import java.util.*;
 
 /**
  *
@@ -17,29 +21,74 @@ public final class StationToStation implements Protocol
     static private int MinPlayer        = 2; // Minimal number of players
     static private int MaxPlayer        = 2; // Maximal number of players
     static private String NameOfTheGame = "ABBA";
-    private Communicator Com;
+    private Communicator com;
+
+    public static final BigInteger ZERO = BigInteger.ZERO;
+    public static final BigInteger ONE = BigInteger.ONE;
+    public static final BigInteger NONE = BigInteger.ONE.negate();
+    public static final BigInteger TWO = BigInteger.valueOf(2L);
+    public static final BigInteger THREE = BigInteger.valueOf(3L);
 
     public void setCommunicator(Communicator com)
     {
-        Com = com;
+        this.com = com;
     }
 
     /** This ia Alice. */
     public void sendFirst () {
-        // p = prim
-        // g = prime wurzel mod p
-        // x_a \in Z_p
-        // y_a = g^{x_a} mod p
+        Random rand = new Random();
+
+        BigInteger p, g, x_a, y_a; {
+
+            // p = prime
+            BigInteger q;
+            do {
+                q = new BigInteger(511, rand); // p = random 512 bit number
+                p = q.multiply(TWO).add(ONE); // p = 2q+1
+            } while(!p.isProbablePrime(42));
+
+            // g = primitive wurzel mod p
+
+            // same algorithm to find a generator
+            BigInteger pMinusOne = p.subtract(ONE);
+            do {
+                // choose 2 < g < q, we should have a 50% probability of hitting a generating number here.
+                g = BigIntegerUtil.randomBetween(THREE, pMinusOne, rand);
+                // check if the required criteria for a generator of G applies
+            } while(!p.modPow(q, p).equals(pMinusOne));
+
+            // x_a \in Z_p
+            x_a = BigIntegerUtil.randomBetween(TWO, p.subtract(TWO));
+
+            // y_a = g^{x_a} mod p
+            y_a = g.modPow(x_a, p);
+
+        }
 
         // send p, g, y_a
+        com.sendTo(2, p.toString());
+        com.sendTo(2, g.toString());
+        com.sendTo(2, y_a.toString());
 
-        // receive y_b, cert_b, xm_b
+        BigInteger K; {
 
-        // CHECK(cert_b)
-        // K = y_b^{x_a} mod p
-        // m_b = UNIDEA(K, xm_b)
-        // test m_b == HASH(y_b*p + y_a)
-        // xm_a = IDEA(K, HASH(y_a*p + y_b)
+            // receive y_b, cert_b, xm_b
+            BigInteger y_b = new BigInteger(com.receive());
+            BigInteger cert_b = new BigInteger(com.receive());
+            BigInteger xm_b = new BigInteger(com.receive());
+
+            // CHECK(cert_b)
+
+            // K = y_b^{x_a} mod p
+            K = y_b.modPow(x_a, p);
+
+            // m_b = UNIDEA(K, xm_b)
+
+            // test m_b == HASH(y_b*p + y_a)
+
+            // xm_a = IDEA(K, HASH(y_a*p + y_b)
+
+        }
 
         // send cert_a, xm_a
 
