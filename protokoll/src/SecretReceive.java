9c9bff1da34bb2511f58329d454cab950b131c67
Vincent Breitmoser <valodim@mugenguild.com>
2013-06-28 16:37:17 +0200

    ss: Secret{Send,Receive} done. awesome piece of class.

--- protokoll/src/SecretReceive.java
+++ protokoll/src/SecretReceive.java
@@ -37,15 +37,15 @@ class SecretReceive {
 
         }
         // We should have exactly arr.length places filled (no nulls left!)
-        assert(j == arr.length);
-        arr2 = arr;
+        assert(j == arr.length) : "Backing array not completely filled in!";
+        arr = arr2;
 
     }
 
     public void notY(int i) {
 
         // WE'VE BEEN HAD!
-        assert(arr[i] != null);
+        assert(arr[i] != null) : "Tried to remove one element twice!";
         // remove index from array
         arr[i] = null;
 
@@ -56,14 +56,76 @@ class SecretReceive {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] != null) {
                 // This MUST be empty at this point!
-                assert(ret == null);
+                assert(ret == null) : "More than one value left, cannot solve (more notY() calls needed)!";
                 ret = arr[i];
             }
         }
 
         // This MUST be non-empty at this point!
-        assert(ret == null);
+        assert(ret != null) : "No value left to return! (should never happen)";
         return ret;
+    }
+
+    public static void main(String[] args) {
+        BigInteger secwet = new BigInteger("1234");
+        System.out.println("in: " + secwet.toString(2));
+        System.out.println();
+
+        int bitlen = secwet.bitLength();
+        int k = 3, k2 = 3;
+
+        SecretSend s = new SecretSend(secwet, k, bitlen);
+        SecretReceive r = new SecretReceive(k, bitlen);
+
+        while(true) {
+            for(int i = 0; i < Math.pow(2, k); i++) {
+                int y = s.y();
+                r.notY(y);
+            }
+
+            if(k2 == bitlen)
+                break;
+
+            System.out.println("next round: " + k2);
+
+            System.out.print("receive: ");
+            for(int i = 0; i < r.arr.length; i++) {
+                if(r.arr[i] == null)
+                    System.out.print("NULL, ");
+                else
+                    System.out.print(r.arr[i].toString(2) + ", ");
+            }
+            System.out.println();
+
+            System.out.print("send: ");
+            for(int i = 0; i < s.arr.length; i++) {
+                if(s.arr[i] == null)
+                    System.out.print("NULL, ");
+                else {
+                    if(s.cur == i)
+                        System.out.print("[" + s.arr[i].toString(2) + "], ");
+                    else
+                        System.out.print(s.arr[i].toString(2) + ", ");
+                }
+            }
+            System.out.println();
+
+            s.nextRound();
+            r.nextRound();
+
+
+            k2 += 1;
+        }
+
+        System.out.println("giving " + (int) (Math.pow(2, k)-1) + " additional bits...");
+        for(int i = 0; i < Math.pow(2, k)-1; i++) {
+            int y = s.yOverride();
+            r.notY(y);
+        }
+
+        System.out.println();
+        System.out.println("done! last element left: " + r.solve().toString(2));
+
     }
 
 }
--- protokoll/src/SecretSend.java
+++ protokoll/src/SecretSend.java
@@ -9,12 +9,12 @@ class SecretSend {
     final int bitlen;
 
     // The initial prefix length.
-    final int k;
+    final int k, twoToK;
     // The current prefix length.
     int k2;
 
     // The (index of the) currently prefix proper.
-    private int cur;
+    int cur;
 
     // Private counter. Counter for money. Even the old songs will do~
     // This counts how many y() calls there have been since the last nextRound().
@@ -28,20 +28,21 @@ class SecretSend {
 
     public SecretSend(BigInteger word, int k, int bitlen) {
         this.k2 = this.k = k;
+        this.twoToK = (int) Math.pow(2, k);
         this.bitlen = bitlen;
         this.word = word;
 
         this.counter = 0;
 
         arr = new BigInteger[(int) Math.pow(2, k+1)];
-        BigInteger prefix = word.xor(word.shiftRight(k).shiftLeft(k));
+        BigInteger prefix = word.shiftRight(word.bitLength()-k2);
         cur = -1;
         for(int i = 0; i < arr.length; i++) {
             arr[i] = BigInteger.valueOf(i);
             if(arr[i].equals(prefix))
                 cur = i;
         }
-        assert(cur != -1 && arr[cur].equals(prefix));
+        assert(cur != -1 && arr[cur].equals(prefix)) : "Prefix not found in initial backing array!";
     }
 
     /// Increases the prefix length by 1, and calculates new backing array.
@@ -49,7 +50,8 @@ class SecretSend {
     // exactly this.k/2 times since the last call to nextRound()!
     public void nextRound() {
 
-        assert(this.counter == this.k/2);
+        assert(this.counter == twoToK) : "Tried to call nextRound() without calling y() exactly 2^k times first!";
+        assert(this.counter != -1) : "Tried to call nextRound() after call to yOverride()!";
 
         this.k2 += 1;
         this.counter = 0;
@@ -57,7 +59,7 @@ class SecretSend {
         this.cur = -1;
 
         // The prefix to check against for the new value of cur.
-        BigInteger prefix = word.xor(word.shiftRight(k2).shiftLeft(k2));
+        BigInteger prefix = word.shiftRight(word.bitLength()-k2);
 
         BigInteger[] arr2 = new BigInteger[arr.length];
         int j = 0;
@@ -70,16 +72,16 @@ class SecretSend {
                 cur = j;
             j += 1;
 
-            arr2[j] = arr2[j-1].setBit(0);
+            arr2[j] = arr[i].shiftLeft(1).setBit(0);
             if(arr2[j].equals(prefix))
                 cur = j;
             j += 1;
 
         }
         // We should have exactly arr.length places filled (no nulls left!)
-        assert(j == arr.length);
-        assert(j != -1 && arr[cur].equals(prefix));
-        arr2 = arr;
+        assert(j == arr.length) : "Backing array not completely filled!";
+        arr = arr2;
+        assert(cur != -1 && arr[cur].equals(prefix)) : "Prefix not marked for next round!";
 
     }
 
@@ -87,7 +89,7 @@ class SecretSend {
     public int y() {
 
         // Before anything else, check if we have not yet returned more than half the array!
-        assert(this.counter < this.k/2);
+        assert(this.counter < twoToK) : "Tried to call y() more than 2^k times between rounds!";
 
         this.counter += 1;
 
@@ -102,5 +104,20 @@ class SecretSend {
 
     }
 
+    /// Returns more ys, non-randomly and regardless of stuff.
+    // There cannot be any more calls to nextRound() after this method has been used!
+    public int yOverride() {
+        this.counter = -1;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(i == cur || arr[i] == null)
+                continue;
+            arr[i] = null;
+            return i;
+        }
+
+        assert(false) : "Tried to get y with empty backing array (ie, only prefix left)!";
+        return 0;
+    }
 
 }
